5443c85df88849eaa2ba44586468631fa0000acb
Emil <emiliano.milewski@gmail.com>
2021-12-28 16:36:22 +0000

    Made Contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,25 @@
+import "./Contact.css";
+
 export default function Contact() {
     return (
-        <h1>Yo from Contact</h1>
+        <div className="contact">
+            <h1 className="contact__title">Email Us</h1>
+            <form className="contact__form">
+                <div className="form__field">
+                    <input className="input" placeholder=" " autoComplete="off" id="name" name="name" type="text" />
+                    <label className="label" htmlFor="name">Name...</label>
+                </div>
+                <div className="form__field">
+                    <input className="input" placeholder=" " autoComplete="off" id="email" name="email" type="email" />
+                    <label className="label" htmlFor="email">Email...</label>
+                </div>
+                <div className="form__field">
+                    <textarea className="input" placeholder=" " autoComplete="off" id="message" name="message" />
+                    <label className="label" htmlFor="message">Your Message...</label>
+                </div>
+                <button className="form__btn" type="submit" >Submit</button>
+            </form>
+        </div>
+
     )
 }
