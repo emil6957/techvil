5339b8a1a9fc1955ac5f84d258be741337c8aba8
Emil <emiliano.milewski@gmail.com>
2022-01-05 18:43:48 +0000

    created checkout button in cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,12 +1,17 @@
 import "./Cart.css";
 
 export default function Cart({ cartItems }) {
-    const cartItemElements = cartItems.map(item => <p key={item.id}>{item.name} x1</p>)
+    const cartItemElements = cartItems.map(item => <div className="flex"><p className="cart-item" key={item.id}>{item.name}</p><p>x1</p></div>)
 
     return (
         <div className="overlay" >
             <div className="cart">
                 {cartItemElements}
+                <div className="flex" >
+                    <p>Total</p>
+                    <p>£550.98</p>
+                </div>
+                <button className="cart__button">Checkout</button>
             </div>
         </div>
     )
