665c435e90bd70d53c307475b920f4b74647814f
Emil <emiliano.milewski@gmail.com>
2022-01-09 14:18:52 +0000

    Cart UI shows ammount of item

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,7 +2,7 @@ import "./Cart.css";
 import { Link } from "react-router-dom";
 
 export default function Cart({ cartItems }) {
-    const cartItemElements = cartItems.map(item => <div key={item.id} className="flex"><p className="cart-item">{item.name}</p><p>x1</p></div>)
+    const cartItemElements = cartItems.map(item => <div key={item.id} className="flex"><p className="cart-item">{item.name}</p><p>x{item.ammount}</p></div>)
 
     return (
         <div className="overlay" >
