95dc223e9b299f72df332f84edf663b0b6f5c280
Emil <emiliano.milewski@gmail.com>
2022-01-09 15:05:44 +0000

    Changed checkout to use the variables

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -5,20 +5,25 @@ export default function Checkout({ cartItems }) {
         return (
             <tr key={item.id}>
                 <td className="table__item">{item.name}</td>
-                <td className="table__quantity">x1</td>
-                <td className="table__price">£{item.price}</td>
+                <td className="table__ammount">x{item.ammount}</td>
+                <td className="table__price">£{item.price * item.ammount}</td>
             </tr>
         )
     })
 
     return (
         <div className="checkout">
+            <form>
+                <input type="text" placeholder="First Name" />
+                <input type="text" placeholder="Last Name" />
+                <input type="email" placeholder="Email" />
+            </form>
             <table className="checkout__table">
                 <thead className="table__head">
                     <tr>
                         <th className="table__item">Item</th>
-                        <th className="table__quantity">Quantity</th>
-                        <th className="table__price">Price</th>
+                        <th className="table__ammount">Quantity</th>
+                        <th className="table__price">Subtotal</th>
                     </tr>
                 </thead>
                 <tbody>
