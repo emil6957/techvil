f3783674787fd38919f0d5b638b80068b4a3d098
Emil <emiliano.milewski@gmail.com>
2022-05-14 16:22:14 +0100

    added a animation for closing the cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,7 +2,7 @@ import "./Cart.css";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-export default function Cart({ cartItems }) {
+export default function Cart({ displayCart, cartItems }) {
     const [total, setTotal] = useState(0);
     const cartItemElements = cartItems.map(item => <div key={item.id} className="flex"><p className="cart-item">{item.name}</p><p>x{item.ammount}</p></div>)
 
@@ -10,12 +10,16 @@ export default function Cart({ cartItems }) {
         setTotal(Math.round(cartItems.reduce((acc, currentItem) => acc + (currentItem.price * currentItem.ammount), 0) * 100) / 100);
     }, [cartItems]);
 
+    const styles = {
+        display: displayCart ? "flex" : "flex",
+    }
+
     return (
-        <div className="overlay" >
+        <div style={styles} className={`overlay ${displayCart ? "" : "closing"}`} >
             {
             cartItemElements.length 
             ?
-            <div className="cart">
+            <div className={`cart ${displayCart ? "" : "closing"}`}>
                 {cartItemElements}
                 <div className="flex" >
                     <p>Total</p>
@@ -24,7 +28,7 @@ export default function Cart({ cartItems }) {
                 <Link className="cart__button" to="/shop/checkout">Checkout</Link>
             </div>
             :
-            <div className="cart center">
+            <div className={`cart center ${displayCart ? "" : "closing"}`}>
                 <p>No Items Currently In Cart</p>
             </div>
             }
